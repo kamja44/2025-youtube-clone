47a880e86761f5854c12377c49b36b1d97243404
kamja44 <88872409+kamja44@users.noreply.github.com>
2025-06-15 12:54:00 +0900

    TRPC init

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,3 +1,18 @@
-export default function Home() {
-  return <div>I will load videos in the future</div>;
+import { Suspense } from "react";
+import { HydrateClient, trpc } from "../trpc/server";
+import { PageClient } from "./client";
+import { ErrorBoundary } from "react-error-boundary";
+
+export default async function Home() {
+  void trpc.hello.prefetch({ text: "Kamja" });
+
+  return (
+    <HydrateClient>
+      <Suspense fallback={<div>Loading...</div>}>
+        <ErrorBoundary fallback={<div>Something went wrong</div>}>
+          <PageClient />
+        </ErrorBoundary>
+      </Suspense>
+    </HydrateClient>
+  );
 }
